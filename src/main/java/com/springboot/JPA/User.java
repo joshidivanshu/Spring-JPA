8102a1b24ad0478153fe13db78d5d26d3c76fb2d
Divanshu Joshi <joshidivanshu16@gmail.com>
2023-12-14 23:22:00 +0530

    added bootstrap

--- src/main/java/com/springboot/JPA/User.java
+++ src/main/java/com/springboot/JPA/User.java
@@ -10,6 +10,7 @@ public class User {
     @Id
     private int id;
     private String name;
+    private String tech;
 
     public int getId() {
         return id;
@@ -27,11 +28,20 @@ public class User {
         this.name = name;
     }
 
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
+                ", tech='" + tech + '\'' +
                 '}';
     }
 }
